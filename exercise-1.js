eee04d2e2e764297dbf310abae2ffd563b401c5c
Louis Marcell Siegl <louis.marcel.siegl@hotmail.it>
2023-03-09 12:02:09 +0100

    exercise 5

---
+++ exercise-1.js
@@ -0,0 +1,16 @@
+const person1 = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 25,
+};
+
+const person2 = person1;
+
+person2["firstName"] = "Simon";
+
+// il valore firstname tra person1 e person2 sono gli stessi perché stiamo il valore della variabile e non il riferimento
+
+// Modify the property `firstName` of the `person2` in "Simon".
+
+console.log(person1);
+console.log(person2);
