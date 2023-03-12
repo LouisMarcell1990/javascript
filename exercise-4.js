e25fa9d8e3efc0a9dd2bb0c83813211182beee7f
Louis Marcell Siegl <louis.marcel.siegl@hotmail.it>
2023-03-12 08:33:10 +0100

    exercise 14

---
+++ exercise-4.js
@@ -0,0 +1,68 @@
+function uncompletedNotes(notes) {
+  
+  let notesId = [];
+
+  let done = [];
+
+  notes.forEach((e) => { 
+    notesId.push(e);
+     notesId.forEach((id) => {
+      done.push(id.todos);
+       
+    })});done.filter((notes) => notes.done === false);
+return notes
+}
+
+/* non so come andare avanti*/
+
+const notes = [
+  {
+    id: 1,
+    description: "Workout program",
+    todos: [
+      {
+        id: 1,
+        name: "Push ups - 10 x 3",
+        done: false,
+      },
+      {
+        id: 2,
+        name: "Abdominals - 20 x 3",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Tapis Roulant - 15min",
+        done: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    description: "Front-end Roadmap",
+    todos: [
+      {
+        id: 1,
+        name: "Learn HTML",
+        done: true,
+      },
+      {
+        id: 2,
+        name: "Learn CSS",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Learn JavaScript",
+        done: true,
+      },
+      {
+        id: 4,
+        name: "Learn Angular",
+        done: false,
+      },
+    ],
+  },
+];
+
+console.log(uncompletedNotes(notes));
