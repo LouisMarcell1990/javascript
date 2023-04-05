3baeff2b0a30d5666beabae9241ad90879a113bf
Louis Marcell Siegl <louis.marcel.siegl@hotmail.it>
2023-04-05 11:35:36 +0200

    exercise 38

---
+++ exercise.js
@@ -0,0 +1,30 @@
+const isLogged = true;
+
+function logged(random) {
+    return new Promise ((resolve, reject) => {
+    let rand = Math.random();
+    if (rand > 0.5) {
+        resolve(random)
+    } else {
+        reject(new Error ('Is minor'))
+    }
+})
+};
+
+function num(userData) {
+    return new Promise((resolve, reject) => {
+    if (userData > 0.5) {
+        resolve({name: "John", age: 24})
+    } else {
+        reject(new Error ('Error'))
+    }
+})
+}
+
+logged(isLogged)
+    .then((val) => num(val))
+    .then((val) => console.log(val))
+    .catch((e) => console.error(e))
+
+
+
